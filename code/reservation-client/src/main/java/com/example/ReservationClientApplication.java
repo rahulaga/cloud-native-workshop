535c4abb31cd3a321e8b8cdea0de664390272f8c
Rahul Agarwal <Rahul.X.Agarwal@kohls.com>
2016-09-14 16:22:20 -0700

    simplifying client

--- code/reservation-client/src/main/java/com/example/ReservationClientApplication.java
+++ code/reservation-client/src/main/java/com/example/ReservationClientApplication.java
@@ -1,79 +1,63 @@
 package com.example;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.hateoas.Resources;
-import org.springframework.http.HttpMethod;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.stream.Collectors;
 
 
-@EnableResourceServer
-@EnableBinding(ReservationChannels.class)
+//@EnableResourceServer
+//@EnableBinding(ReservationChannels.class)
 @EnableZuulProxy
-@EnableCircuitBreaker
+//@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationClientApplication {
 
-	@Bean
-	@LoadBalanced
+/*	@Bean
+	//@LoadBalanced
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-
+*/
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationClientApplication.class, args);
 	}
 }
 
+/*
 interface ReservationChannels {
 
 	@Output
 	MessageChannel output();
 }
+*/
 
+
+/*
 
 @RestController
 @RequestMapping("/reservations")
 class ReservationServiceApiGatewayRestController {
 
-	private final MessageChannel channel;
+	//private final MessageChannel channel;
 	private final RestTemplate restTemplate;
 
 	@RequestMapping(method = RequestMethod.POST)
 	public void write(@RequestBody Reservation reservation) {
-		this.channel.send(MessageBuilder.withPayload(reservation.getReservationName()).build());
+		//this.channel.send(MessageBuilder.withPayload(reservation.getReservationName()).build());
 	}
 
 	@Autowired
 	public ReservationServiceApiGatewayRestController(
 			@LoadBalanced RestTemplate restTemplate, ReservationChannels channels) {
 		this.restTemplate = restTemplate;
-		this.channel = channels.output();
+		//this.channel = channels.output();
 	}
 
 
+			
+			
 	public Collection<String> fallback() {
 		return new ArrayList<>();
 	}
@@ -104,3 +88,4 @@ class Reservation {
 		return reservationName;
 	}
 }
+*/
