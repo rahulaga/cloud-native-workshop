3c9932aed8a39e761f0a734e0a6ad6089f2ee4e6
Rahul Agarwal <Rahul.X.Agarwal@kohls.com>
2016-09-14 13:38:46 -0700

    updating poms

--- code/reservation-service/src/main/java/demo/ReservationServiceApplication.java
+++ code/reservation-service/src/main/java/demo/ReservationServiceApplication.java
@@ -1,31 +1,27 @@
 package demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.integration.annotation.MessageEndpoint;
-import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.util.Arrays;
-import java.util.Collection;
-
-@EnableBinding(Sink.class)
+//@EnableBinding(Sink.class)
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
@@ -48,7 +44,7 @@ public class ReservationServiceApplication {
     }
 }
 
-@MessageEndpoint
+/*@MessageEndpoint
 class MessageReservationReceiver {
 
     @ServiceActivator(inputChannel = Sink.INPUT)
@@ -58,7 +54,7 @@ class MessageReservationReceiver {
 
     @Autowired
     private ReservationRepository reservationRepository;
-}
+}*/
 
 @RefreshScope
 @RestController
