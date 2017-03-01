28e8c01639f64be0385f130473fb301879d2e964
rahul <rahulaga@gmail.com>
2017-03-01 15:47:47 -0800

    adding access logging

--- config-service/src/main/java/com/example/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/ConfigServiceApplication.java
@@ -2,7 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+
+import ch.qos.logback.access.tomcat.LogbackValve;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -10,5 +15,14 @@ public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+	
+	@Bean
+	public EmbeddedServletContainerFactory servletContainerFactory(){
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+		LogbackValve logbackValve = new LogbackValve();
+		logbackValve.setFilename("logback-access.xml");
+		tomcat.addContextValves(logbackValve);
+		return tomcat;
 	}
 }
