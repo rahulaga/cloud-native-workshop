82808598a94257ca518529ea2bac627f3142dbdd
rahul <rahulaga@gmail.com>
2017-03-01 16:57:04 -0800

    adding access log, configs

--- eureka-service/src/main/java/com/example/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/example/EurekaServiceApplication.java
@@ -2,7 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import ch.qos.logback.access.tomcat.LogbackValve;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -10,5 +15,14 @@ public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+	
+	@Bean
+	public EmbeddedServletContainerFactory servletContainerFactory(){
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+		LogbackValve logbackValve = new LogbackValve();
+		logbackValve.setFilename("logback-access.xml");
+		tomcat.addContextValves(logbackValve);
+		return tomcat;
 	}
 }
