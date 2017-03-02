42a03e99299e9946cbd9214010a1114d2c429795
rahul <rahulaga@gmail.com>
2017-03-01 18:13:53 -0800

    register config service with eureka

--- config-service/src/main/java/com/example/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/ConfigServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import ch.qos.logback.access.tomcat.LogbackValve;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
